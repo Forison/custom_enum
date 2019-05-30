17ee70202c7a4cae77716cf80c8d534197ef9de6
unknown <forison6205@gmail.com>
2019-05-30 10:16:37 +0000

    commit custom my_each_with_index enum method

--- enum_methods.rb
+++ enum_methods.rb
@@ -1,17 +1,18 @@
-module Enumerable
+# module Enumerable
     def my_each(arg)
       # your code here
       if arg.class==Array
       0.upto(arg.size-1){|i| yield arg[i]}
       end
       if arg.class==Hash
-       0.upto(arg.size-1){|i| yield arg.keys[i] ,arg.values[i]} 
+      (arg.length).times{|i| yield arg.keys[i] ,arg.values[i]} 
       end
     end
-end
-# a=[2,3]
-# h={2=> 'we',
-#     3=> 'wew'
-# }
 
-# puts my_each(h){|a| puts a}
+    def my_each_with_index(arg)
+      (arg.length-1).times{|i| yield i}
+    end
+# end
+a=[2,3]
+
+puts my_each_with_index(a){|a,n| puts a}
