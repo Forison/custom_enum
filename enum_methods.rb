cffa5f28e510d48b4a0e079f5047b8bf3e142ebb
unknown <forison6205@gmail.com>
2019-06-04 00:32:04 +0000

    commit my_selct enum method

--- enum_methods.rb
+++ enum_methods.rb
@@ -10,9 +10,46 @@
     end
 
     def my_each_with_index(arg)
-      (arg.length-1).times{|i| yield i}
+      (arg.length-1).times{|i| yield arg[i],i}
     end
-# end
-a=[2,3]
 
-puts my_each_with_index(a){|a,n| puts a}
+  def my_select
+   if block_given?
+   if self.class ==Array
+    temp=[]
+    (self.size-1).times do |x|
+        yield temp.push(x)
+        return temp
+    end
+   end
+   if self.class==Hash
+    temp={}
+    (self.size-1).times do |key,value|
+        yield temp[key]=value
+        return temp
+    end
+   end
+else
+    "this method requires a block"
+end
+
+
+def my_all?(*arg)
+#begin if
+if block_given? 
+    self.each do |i| yield self[i] end
+else
+    self.each do |obj| 
+         return false if obj ==false || obj==nil
+         return true
+        end
+    self.size==0?true:true
+end
+# end of if statement
+end
+
+
+end
+# end
+a=[nil]
+puts a.my_all?
